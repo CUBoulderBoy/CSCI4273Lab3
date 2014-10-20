30e52593c9ff3cd5862ab4fae982d5b9ef3ff5ba
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-10-20 16:42:44 -0600

    add EventScheduler.h

---
+++ EventScheduler.h
@@ -0,0 +1,9 @@
+
+class EventScheduler
+{
+public:
+    EventScheduler(size t maxEvents)
+    ∼EventScheduler()
+    int eventSchedule(void evFunction(void *), void *arg, int timeout)
+    void eventCancel(int eventId)
+};
